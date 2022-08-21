7a94bc3dd61aaafcd2ceeda13ea0269d8ff98f39
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-08-21 15:50:29 +0500

    Задача2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int number1 = new Random().Next(1, 10);
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 10);
+Console.WriteLine(number2);
+
+
+
+int max = number1;
+
+if (max < number2 || max == number2) max = number2;
+else
+{
+    max = number1;
+}
+Console.WriteLine(max);
