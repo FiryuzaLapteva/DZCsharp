8a264472c2c56f214a3174a4b48444c1e311e3b1
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-08-21 15:58:25 +0500

    Задача4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int number1 = new Random().Next(1, 100);
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 100);
+Console.WriteLine(number2);
+int number3 = new Random().Next(1, 100);
+Console.WriteLine(number3);
+
+int max = number1;
+
+if (max < number2 || max == number2) max = number2;
+if (max < number3 || max == number3) max = number3;
+else
+{
+    max = number1;
+}
+Console.WriteLine(max);
