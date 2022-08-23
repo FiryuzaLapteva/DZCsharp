008c473f7d786a5a721b3b63de18fe2dd1930582
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-08-24 00:43:17 +0500

    Задача13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int number1 = new Random().Next(10, 1000);
+//int number1 = 72;
+Console.WriteLine(number1);
+
+if (number1 == 0 || number1 < 100)
+{
+    Console.WriteLine("Третьей цифры нет. ");
+}
+else
+{
+int number2 = (number1%100)%10;
+Console.WriteLine(number2); 
+}
+
