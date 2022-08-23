dd0644e5c97b5099688c68a4a3f586a29fbb3cdd
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-08-24 00:22:16 +0500

    Задача10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+int number1 = new Random().Next(100, 1000);
+Console.WriteLine(number1);
+int number2 = number1%100;
+//Console.WriteLine(number2); 
+int result = number2/10;
+
+Console.WriteLine("Вторая цифра = "); 
+Console.WriteLine(result);
