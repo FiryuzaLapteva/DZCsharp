ea2fa77a5fbca289bfc87ff9039caa24bb6f4400
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-09-05 16:39:08 +0500

    Задачи от 5 семинара

---
+++ Zadacha36/Program.cs
@@ -0,0 +1,13 @@
+﻿// ============Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] array = new int[5];
+int sum =0;
+for(int i = 0; i<array.Length; i++)
+{
+    array[i]=new Random().Next(100,1000);
+    if(i%2!=0)
+    {
+        sum+=array[i];
+    }
+}
+ Console.WriteLine(String.Join( ";", array));
+ Console.WriteLine("сумма элементов, стоящих на нечётных позициях = " + sum);
---
+++ Zadacha38/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] array = new int[5];// Инициируем массив размером 5 элементов
+for(int i = 0; i<array.Length; i++)
+{ 
+    array[i]=new Random().Next(-100,100);
+}
+ Console.WriteLine(String.Join( ";", array));
+
+int NumberMax (int[] num){ //Ищем максимальное число в массиве
+int max = num[0];
+for (int j = 1; j < num.Length; j++)
+{
+    if (max < num[j])
+    { 
+        max = num [j];
+    }
+}
+return max;
+}
+Console.WriteLine(NumberMax(array));
+
+int NumberMin (int[] num){ //Ищем минимальное число в массиве
+int min = num[0];
+for (int j = 1; j < num.Length; j++)
+{
+    if (min > num[j])
+    { 
+        min = num [j];
+    }
+}
+return min;
+}
+Console.WriteLine(NumberMin(array));
+
+Console.WriteLine("Максимальное - Минимальное = " + (NumberMax(array) - NumberMin(array)));
