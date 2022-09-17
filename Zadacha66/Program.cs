83da17cd76672d49348b1e4650a1c9a9ca6f1f6f
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-09-18 00:38:02 +0500

    Задачи от 9 семинара

--- Zadacha66/Program.cs
+++ Zadacha66/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N.
+//  Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int M = 5;
+int N = 7;
+ int Summa (int M, int N)
+ {
+    if (M == N) return M;
+    return (M + Summa(M + 1, N));
+ }
+ Console.WriteLine(Summa(M,N));
+ Console.WriteLine();
